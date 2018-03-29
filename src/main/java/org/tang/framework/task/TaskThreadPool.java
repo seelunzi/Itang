13ae5558d1094de42321a3275b2104609da597c0
tangwenbo <418304914@qq.com>
2018-03-29 16:31:14 +0800

    优化代码

--- src/main/java/org/tang/framework/task/TaskThreadPool.java
+++ src/main/java/org/tang/framework/task/TaskThreadPool.java
@@ -1,10 +1,28 @@
 package org.tang.framework.task;
 
-import java.util.concurrent.ScheduledThreadPoolExecutor;
+import com.google.appengine.repackaged.com.google.common.util.concurrent.ThreadFactoryBuilder;
 
+import java.util.concurrent.*;
+
+/**
+ * @author tang
+ */
 public class TaskThreadPool {
 
     public static final ScheduledThreadPoolExecutor taskPool = new ScheduledThreadPoolExecutor(30);
 
+    /***
+     * 带名字的线程池
+     * */
+    public void ceshi() {
+        ThreadFactory namedThreadFactory = new ThreadFactoryBuilder()
+                .setNameFormat("demo-pool-%d").build();
+        ExecutorService singleThreadPool = new ThreadPoolExecutor(1, 1,
+                0L, TimeUnit.MILLISECONDS,
+                new LinkedBlockingQueue<Runnable>(1024), namedThreadFactory, new ThreadPoolExecutor.AbortPolicy());
+
+        singleThreadPool.execute(() -> System.out.println(Thread.currentThread().getName()));
+        singleThreadPool.shutdown();
+    }
 
 }
